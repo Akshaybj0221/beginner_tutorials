5a0a2384ca992a9f3ac50d4b444ca5704be5a3f8
Akshay <akshaybj0221@gmail.com>
2017-11-06 23:29:58 -0800

    add two ints tutorial successful build

--- src/add_two_ints_client.cpp
+++ src/add_two_ints_client.cpp
@@ -1,23 +1,30 @@
 #include "ros/ros.h"
 #include "beginner_tutorials/AddTwoInts.h"
-
-bool add(beginner_tutorials::AddTwoInts::Request  &req,
-         beginner_tutorials::AddTwoInts::Response &res)
-{
-  res.sum = req.a + req.b;
-  ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
-  ROS_INFO("sending back response: [%ld]", (long int)res.sum);
-  return true;
-}
+#include <cstdlib>
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "add_two_ints_server");
-  ros::NodeHandle n;
+  ros::init(argc, argv, "add_two_ints_client");
+  if (argc != 3)
+  {
+    ROS_INFO("usage: add_two_ints_client X Y");
+    return 1;
+  }
 
-  ros::ServiceServer service = n.advertiseService("add_two_ints", add);
-  ROS_INFO("Ready to add two ints.");
-  ros::spin();
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
+  beginner_tutorials::AddTwoInts srv;
+  srv.request.a = atoll(argv[1]);
+  srv.request.b = atoll(argv[2]);
+  if (client.call(srv))
+  {
+    ROS_INFO("Sum: %ld", (long int)srv.response.sum);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service add_two_ints");
+    return 1;
+  }
 
   return 0;
 }
