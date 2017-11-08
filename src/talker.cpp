6fd2cbda40f865d1ce0e080c19437912169d41a1
Akshay <akshaybj0221@gmail.com>
2017-11-07 17:57:33 -0800

    Added cpplint text file

--- src/talker.cpp
+++ src/talker.cpp
@@ -58,7 +58,7 @@ bool add(beginner_tutorials::text::Request& req,
  *
  * @return     nothing
  */
-int main (int argc, char **argv) {
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -69,10 +69,10 @@ int main (int argc, char **argv) {
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
-  ros::init (argc, argv, "talker");
+  ros::init(argc, argv, "talker");
 
   // setting default frequency to 10 
-  //(It can be changed using argument in launch file)
+  // (It can be changed using argument in launch file)
   int freq = 10;
 
   // Checking number of arguments
