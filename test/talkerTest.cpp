69eb0efe0eeed417d212e3ed5a26fa061d618e65
Akshay <akshaybj0221@gmail.com>
2017-11-14 21:22:17 -0800

    Added tf test in talkertest to check its working

--- test/talkerTest.cpp
+++ test/talkerTest.cpp
@@ -13,3 +13,17 @@ TEST(TestService, test_existence) {
    bool exists(client.waitForExistence(ros::Duration(10)));
    EXPECT_TRUE(exists);
 }
+
+TEST(testTf, tfWorking) {
+   ros::NodeHandle nh1;
+
+   tf::TransformListener tflistener;
+   tf::StampedTransform transform;
+
+   try {
+      EXPECT_NO_FATAL_FAILURE(listener.waitForTransform("/world", "/talk", ros::Time(0), ros::Duration(0.5)));
+
+      EXPECT_NO_FATAL_FAILURE(listener.lookupTransform("/world", "/talk", ros::Time(0), transform));
+}
+}
+
