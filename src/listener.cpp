f3b42eb038a23ca7b79e9b01b850e0175b619ec2
Akshay <akshaybj0221@gmail.com>
2017-11-14 23:27:22 -0800

    Added doxygen comments and license to listener

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,8 +1,25 @@
-/************************************************************
- *                                                          *
- * Copyright (C) 2017 by Akshay Bajaj                       *
- *                                                          *
- ***********************************************************/
+/**********************************************************************************
+ *                                                          			  *
+ * Copyright (C) 2017 by Akshay Bajaj                       			  *
+ *										  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell	  *
+ * copies of the Software, and to permit persons to whom the Software is	  *
+ * furnished to do so, subject to the following conditions:			  *
+ *										  *
+ * The above copyright notice and this permission notice shall be included in	  *
+ * all copies or substantial portions of the Software.				  *
+ *										  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR	  *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	  *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE	  *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	  *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                         		  *
+ *********************************************************************************/
 
 /**
  *   @file	  listener.cpp
@@ -10,8 +27,7 @@
  *   @copyright   MIT License
  *
  *   @brief  	  Demo for ROS listener node   
- *   @date	  10/30/2017
- *   @updateDate  11/06/2017
+ *   @date	  11/14/2017
  *
  *   @section	  DESCRIPTION
  *
