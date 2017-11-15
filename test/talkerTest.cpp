79cc413a54396d14a93bfe02e7a1197a0c15b427
Akshay <akshaybj0221@gmail.com>
2017-11-15 00:20:22 -0800

    Added doxygen comments and license to testTalker.cpp of test

--- test/talkerTest.cpp
+++ test/talkerTest.cpp
@@ -1,9 +1,51 @@
+/**********************************************************************************
+ *                                                          			  *
+ * Copyright (C) 2017 by Akshay Bajaj                       			  *
+ *										  *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy   *
+ * of this software and associated documentation files (the "Software"), to deal  *
+ * in the Software without restriction, including without limitation the rights   *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell	  *
+ * copies of the Software, and to permit persons to whom the Software is	  *
+ * furnished to do so, subject to the following conditions:			  *
+ *										  *
+ * The above copyright notice and this permission notice shall be included in	  *
+ * all copies or substantial portions of the Software.				  *
+ *										  *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR	  *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,	  *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE	  *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER	  *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  *
+ * SOFTWARE.                                                         		  *
+ *********************************************************************************/
+
+/**
+ *   @file	  talkerTest.cpp
+ *   @author	  Akshay Bajaj
+ *   @copyright   MIT License
+ *
+ *   @brief  	  Demo for ROS talker node   
+ *   @date	  11/14/2017
+ *
+ *   @section	  DESCRIPTION
+ *
+ *   This program is the testing code for the package beginner_tutorials of the beginner tutorials series in ROS */
+
 #include <gtest/gtest.h>
 #include <ros/ros.h>
 #include <ros/service_client.h>
 #include <tf/transform_listener.h>
 #include <sstream>
 #include "beginner_tutorials/text.h"
+
+
+
+
+/**
+ * @brief      To test the service
+ */
 
 TEST(TestService, test_existence) {
    ros::NodeHandle nh;
@@ -13,6 +55,12 @@ TEST(TestService, test_existence) {
    bool exists(client.waitForExistence(ros::Duration(10)));
    EXPECT_TRUE(exists);
 }
+
+
+/**
+ * @brief      To test talker tf broadcaster
+ */
+
 
 TEST(testTf, tfWorking) {
    ros::NodeHandle nh1;
